57a298849398928671e04719054cc6d1e86828de
Courtney Struelens <courtneystruelens@hotmail.com>
2022-07-04 22:10:46 -0700

    wired getting all thoughts

---
+++ controllers/thoughtController.js
@@ -0,0 +1,10 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+  // Get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+};
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const { getThoughts } = require("../../controllers/thoughtController");
+
+// /api/thoughts
+router.route("/").get(getThoughts);
+
+module.exports = router;
